d08a790fcb8c1af12b8892b576f751bc2443b563
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-23 10:48:07 -0300

    localstorage - adding objects and arrays with JSON.stringify

---
+++ Local Storage/js/01-app.js
@@ -0,0 +1,18 @@
+// Como agregar elementos al localStorage
+
+
+// Es un espacio de guardado permanente en el almacenamiento local del cliente. Los datos persistiran hasta que sean limpiados o eliminados.
+localStorage.setItem('nombre', 'Ilan');
+
+const producto = {
+    nombre: 'Monitor 24 Pulgadas',
+    precio: 300,
+}
+// Los datos que almacena el local storage son siempre strings. Por lo que hay forma de guardar objetos o arreglos completos en forma de string utilizando una utilidad que se llama 'stringify' de la libreria de JS 'JSON'.
+
+localStorage.setItem('producto', JSON.stringify(producto));
+
+// Tambien puedo guardar arreglos.
+const meses = ['Enero', 'Febrero', 'Marzo'];
+
+localStorage.setItem('meses', JSON.stringify(meses));
