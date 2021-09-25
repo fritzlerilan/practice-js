a5af8623f09aa0cb740f80a0ee1ecb7eeb927f48
Ilan Emanuel Fritzler <fritzlerilan@gmail.com>
2021-09-25 13:20:54 -0300

    POO - Methods and static methods

--- POO/js/01-app.js
+++ POO/js/01-app.js
@@ -5,10 +5,21 @@ class Cliente {
         this.nombre = nombre;
         this.saldo = saldo;
     }
+
+    mostrarInformacion() {
+        return `Cliente: ${this.nombre} tu saldo es de $${this.saldo}`;
+    }
+
+    static bienvenida() {
+        return `Bienvenido al cajero`;
+    }
 }
+
+console.log(Cliente.bienvenida());
 
 const juan = new Cliente('Juan', 400);
 console.log('juan:', juan)
+console.log(juan.mostrarInformacion());
 
 // Class Expresion
 
@@ -17,7 +28,12 @@ const Cliente2 = class {
         this.nombre = nombre;
         this.saldo = saldo;
     }
+
+    mostrarInformacion() {
+        return `Cliente: ${this.nombre} tu saldo es de $${this.saldo}`;
+    }
 }
 
 const ilan = new Cliente2('Ilan', 500);
 console.log('ilan:', ilan)
+console.log(ilan.mostrarInformacion());
