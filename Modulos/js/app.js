869e3223f4a375935c0feca050b41e400f681ffd
fritzlerilan <fritzlerilan@gmail.com>
2021-09-30 14:01:12 -0300

    modules/export and import functions

--- Modulos/js/app.js
+++ Modulos/js/app.js
@@ -1,4 +1,8 @@
-import { nombreCliente, ahorro } from './cliente.js';
+import { nombreCliente, ahorro, tieneSaldo } from './cliente.js';
+import { mostrarInformacion } from './cliente.js';
 
 console.log(nombreCliente);
 console.log(ahorro);
+
+console.log(mostrarInformacion(nombreCliente, ahorro));
+tieneSaldo(ahorro);
