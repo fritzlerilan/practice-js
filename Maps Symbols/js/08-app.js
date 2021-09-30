2c6e20453e12e082989135a8cc5bf163179e830a
fritzlerilan <fritzlerilan@gmail.com>
2021-09-30 13:45:45 -0300

    maps-symbols/Otros iteradores

---
+++ Maps Symbols/js/08-app.js
@@ -0,0 +1,61 @@
+// Opciones de iteradores
+
+const ciudades = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"];
+const ordenes = new Set([123, 231, 131, 102, 234]);
+const datos = new Map();
+
+datos.set('nombre', 'Ilan');
+datos.set('profesion', 'Desarrollador Full Stack');
+
+// // Iterador entries
+// for (let entry of ciudades.entries() ) {
+//     console.log(entry);
+// }
+
+// for (let entry of ordenes.entries() ) {
+//     console.log(entry);
+// }
+
+// for (let entry of datos.entries() ) {
+//     console.log(entry);
+// }
+
+// // Iterador values
+// for (let value of ciudades.values()){
+//     console.log(value);
+// }
+
+// for (let value of ordenes.values()){
+//     console.log(value);
+// }
+
+// for (let value of datos.values()){
+//     console.log(value);
+// }
+
+// // Itarador Keys
+// for(let keys of ciudades.keys()){
+//     console.log(keys);
+// }
+
+// for(let keys of ordenes.keys()){
+//     console.log(keys);
+// }
+
+// for(let keys of datos.keys()){
+//     console.log(keys);
+// }
+
+
+// // Default iterator
+// for (let ciudad of ciudades){
+//     console.log(ciudad);
+// }
+
+// for (let orden of ordenes){
+//     console.log(orden);
+// }
+
+// for (let dato of datos){
+//     console.log(dato);
+// }
