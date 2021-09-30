c030b1238dffc11478215a273ffa78d81a972e54
fritzlerilan <fritzlerilan@gmail.com>
2021-09-30 14:10:22 -0300

    modules/inheritance and export classes

--- Modulos/js/app.js
+++ Modulos/js/app.js
@@ -1,5 +1,5 @@
-import { nombreCliente, ahorro, tieneSaldo, Cliente } from './cliente.js';
-import { mostrarInformacion } from './cliente.js';
+import { nombreCliente, ahorro, tieneSaldo, Cliente, mostrarInformacion } from './cliente.js';
+import { Empresa } from './empresa.js';
 
 console.log(nombreCliente);
 console.log(ahorro);
@@ -9,3 +9,7 @@ tieneSaldo(ahorro);
 
 const cliente = new Cliente(nombreCliente, ahorro);
 console.log(cliente.mostrarInformacion());
+
+const empresa = new Empresa('Codigo con Juan', 100, 'Aprendizaje en Linea');
+console.log(empresa);
+empresa.mostrarInformacion();
