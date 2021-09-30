af30caba78d1e9ca047422bde0f7eb2f8d673ce1
fritzlerilan <fritzlerilan@gmail.com>
2021-09-30 12:21:49 -0300

    maps-symbols/sets adding data

--- Maps Symbols/js/01-app.js
+++ Maps Symbols/js/01-app.js
@@ -1,6 +1,12 @@
 // Sets
 const carrito = new Set();
 
+// Agregar elementos
+carrito.add('Camisa');
+carrito.add('Disco #1');
+carrito.add('Disco #2');
+carrito.add('Disco #3');
+carrito.add('Camisa'); // Los set no permiten valores duplicados
 
 
 
