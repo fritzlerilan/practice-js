318f9ba17c0ce03b4389126795a207012dcfec4a
fritzlerilan <fritzlerilan@gmail.com>
2021-10-01 10:53:48 -0300

    indexedDb - add a new register

--- IndexDB/js/app.js
+++ IndexDB/js/app.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     initDB();
-})
 
+    setTimeout(() => {
+        crearCliente();
+    }, 5000);
+})
+// Instancia global de la DB
+let DB;
 
 function initDB(){
     const init = window.indexedDB.open('crm', 1);
     // En caso de ser exitosa la creacion de la db
     init.onsuccess = function (e) {
-        console.log(e.target);
+        DB = e.target.result;
     }
     // En caso de error
     init.onerror = function (e) {
@@ -16,7 +21,7 @@ function initDB(){
     // Una vez creada la base de datos este codigo se ejecutara una sola vez
     init.onupgradeneeded = function (e) {
         // Obtengo la DB
-        const db = e.target.result;
+        DB = e.target.result;
         // Creo el Object Store
         const objectStore = db.createObjectStore('crm', {
             keyPath: 'crm',
@@ -28,4 +33,21 @@ function initDB(){
         objectStore.createIndex('email', 'email', {unique: true});
         objectStore.createIndex('telefono', 'telefono', {unique: false});
     }
+}
+// IndexDB funciona con transacciones
+function crearCliente() {
+    let transaction = DB.transaction(['crm'], 'readwrite');
+    transaction.oncomplete = function (){
+        console.log('Transaccion Completada');
+    }
+    transaction.onerror = function() {
+        console.log('Hubo un error en la transaccion');
+    }
+    const objectStore = transaction.objectStore('crm');
+    const nuevoCliente = {
+        telefono: 2236690789,
+        nombre: 'Ilan',
+        email: 'fritzlerilan@gmail.com'
+    }
+    const peticion = objectStore.add(nuevoCliente);
 }
