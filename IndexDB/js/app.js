e5a505402ebe8b05a2aa8871594ae63d680f6754
fritzlerilan <fritzlerilan@gmail.com>
2021-10-01 10:38:21 -0300

    indexedDb - re-writing  a new indexedDB database

--- IndexDB/js/app.js
+++ IndexDB/js/app.js
@@ -1,21 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    crmDB();
+    initDB();
 })
 
-function crmDB(){
-    // Crear base de datos version 1.0
-    let crmDB = window.indexedDB.open('crm', 1);
 
-    // Si hay un error
-    crmDB.onerror = function() {
-        console.log('Hubo un error a la hora de crear la DB');
+function initDB(){
+    const init = window.indexedDB.open('crm', 1);
+    // En caso de ser exitosa la creacion de la db
+    init.onsuccess = function (e) {
+        console.log(e.target);
     }
-    // Si se creo bien
-    crmDB.onsuccess = function(){
-        console.log('Base de datos creada');
+    // En caso de error
+    init.onerror = function (e) {
+        console.log('Hubo un error al crear la DB');
     }
-    // Configuracion de la DB
-    crmDB.onupgradeneeded = function() {
-        console.log('Este metodo solo se ejecuta una vez');
+    // Una vez creada la base de datos este codigo se ejecutara una sola vez
+    init.onupgradeneeded = function (e) {
+        // Obtengo la DB
+        const db = e.target.result;
+        // Creo el Object Store
+        const objectStore = db.createObjectStore('crm', {
+            keyPath: 'crm',
+            autoIncrement: true
+        });
+        // Creo las columnas
+        console.log(objectStore);
+        objectStore.createIndex('nombre', 'nombre', {unique: false});
+        objectStore.createIndex('email', 'email', {unique: true});
+        objectStore.createIndex('telefono', 'telefono', {unique: false});
     }
 }
