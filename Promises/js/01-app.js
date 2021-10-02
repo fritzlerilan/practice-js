bb0973ce11314efe1428e0a0fa759e6f299647b9
fritzlerilan <fritzlerilan@gmail.com>
2021-10-02 14:29:23 -0300

    promises/callbacks - ejemplo con callback

--- Promises/js/01-app.js
+++ Promises/js/01-app.js
@@ -2,6 +2,13 @@
 
 const paises = ['francia', 'españa', 'portugal', 'australia', 'inglaterra'];
 
+function nuevoPais(pais, callback) {
+    setTimeout(() => {
+        paises.push(pais);
+        callback();
+    }, 2000);
+}
+
 function mostrarPaises() {
     setTimeout(() => {
         paises.forEach(pais => {
@@ -12,4 +19,6 @@ function mostrarPaises() {
 
 
 // Si cuando se esta mostrando la lista de clientes se añade un pais nuevo. este no formaria parte de la lista impresa por la funcion mostrarPaises(). Aqui es donde son utiles los callback
+
 mostrarPaises();
+nuevoPais('Alemania', mostrarPaises);
